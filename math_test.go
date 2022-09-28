e1ae4e1c2f25fe58e7cb2cce8281876d71a6f601
Lucas Valbusa <lucasvalbusagit@gmail.com>
2022-09-28 11:29:12 -0300

    fix: change description

--- math_test.go
+++ math_test.go
@@ -6,6 +6,6 @@ func TestSoma(t *testing.T) {
 	total := Soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultaod da soma é inválido. Resultado: %d. Esparado: %d", total, 30)
+		t.Errorf("Resultado da soma é inválido. Resultado: %d. Esparado: %d", total, 30)
 	}
 }
